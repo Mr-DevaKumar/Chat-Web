59d21b03dc07aff231815de4d821353fd005fc03
Deva Kumar <145765159+Mr-DevaKumar@users.noreply.github.com>
2025-01-27 19:05:13 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const PORT = 3000;
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+    console.log("A user connected:", socket.id);
+
+    socket.on("chat message", (msg) => {
+        console.log("Message received:", msg);
+        io.emit("chat message", msg); // Broadcast the message to all clients
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected:", socket.id);
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
